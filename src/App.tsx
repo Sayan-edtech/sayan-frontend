67dd37114744a96dac7b1b274de823383f4e1eee
kareem2002shimes <kareem4shimas@gmail.com>
2025-05-27 02:36:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome Sayan</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
