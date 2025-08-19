a48153b38b828d33183c5ebd0522cc5db8433b6c
kareem-shimes <kareem4shimas@gmail.com>
2025-08-19 13:32:44 +0300

    change VITE_API_URL & remove subdomain routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,36 +10,22 @@ import CourseDetails from "@/pages/courses/[slug]";
 import LaunchAcademy from "@/pages/launch-academy";
 import EmployeeTraining from "@/pages/employee-training";
 import Ai from "@/pages/ai";
-
 import { dashboardRoutes } from "./DashboardRoutes";
 import { authRoutes } from "./AuthRoutes";
 import { academyRoutes } from "./academy-routes";
-import { hasSubdomain } from "@/lib/subdomain";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {hasSubdomain() ? (
-        // Academy subdomain routes
-        <>
-          {academyRoutes}
-          {dashboardRoutes}
-        </>
-      ) : (
-        // Main application routes
-        <>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="courses/:courseSlug" element={<CourseDetails />} />
-          <Route path="launch-academy" element={<LaunchAcademy />} />
-          <Route path="ai" element={<Ai />} />
-          <Route path="employee-training" element={<EmployeeTraining />} />
-
-          {dashboardRoutes}
-          {authRoutes}
-          {academyRoutes}
-        </>
-      )}
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="courses/:courseSlug" element={<CourseDetails />} />
+      <Route path="launch-academy" element={<LaunchAcademy />} />
+      <Route path="ai" element={<Ai />} />
+      <Route path="employee-training" element={<EmployeeTraining />} />
+      {dashboardRoutes}
+      {authRoutes}
+      {academyRoutes}
       <Route path="*" element={<NotFound />} />
     </>
   )
--- src/routes/academy-routes/index.tsx
+++ src/routes/academy-routes/index.tsx
@@ -2,15 +2,8 @@ import Home from "@/templates/template-one/pages";
 import CourseDetails from "@/templates/template-one/pages/courses/[slug]";
 import { Route } from "react-router-dom";
 import { academyAuthRoutes } from "./AuthRoutes";
-import { hasSubdomain } from "@/lib/subdomain";
 
-export const academyRoutes = hasSubdomain() ? (
-  <Route path="/">
-    <Route index element={<Home />} />
-    <Route path="courses/:courseSlug" element={<CourseDetails />} />
-    {academyAuthRoutes}
-  </Route>
-) : (
+export const academyRoutes = (
   <Route path="academy/:academySlug">
     <Route index element={<Home />} />
     <Route path="courses/:courseSlug" element={<CourseDetails />} />
