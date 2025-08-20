e699acba73e2123dc8d714ce215c795516b44b4d
kareem-shimes <kareem4shimas@gmail.com>
2025-08-20 17:47:58 +0300

    enhance sayan dashboard

--- src/components/shared/dashboard/DashboardHeader.tsx
+++ src/components/shared/dashboard/DashboardHeader.tsx
@@ -5,6 +5,8 @@ import { UserMenu } from "./UserMenu";
 import { Link } from "react-router-dom";
 import { useCurrentUserProfile } from "@/features/dashboard/profile/hooks";
 import { getAcademyDetails } from "@/lib/academy";
+import AddItemModal from "./AddItemModal";
+import { NotificationsDropdown } from "./NotificationsDropdown";
 
 interface DashboardHeaderProps {
   onMobileMenuClick: () => void;
@@ -17,22 +19,29 @@ export function DashboardHeader({ onMobileMenuClick }: DashboardHeaderProps) {
     <header className="bg-white border-b border-border">
       <div className="px-4 lg:px-6 py-4">
         <div className="flex items-center justify-between gap-4 lg:gap-6">
-          <UserMenu align="start" />
-          {user?.academy_memberships && (
-            <Link
-              to={`/academy/${getAcademyDetails(user!)?.academy_slug}`}
-              target="_blank"
-            >
-              <Button
-                variant="ghost"
-                size="sm"
-                className="relative"
-                title="معاينة الأكاديمية"
-              >
-                <Eye className="w-5 h-5" />
-              </Button>
-            </Link>
-          )}
+          <div className="flex items-center gap-4">
+            <UserMenu align="start" />
+            {user?.academy_memberships && (
+              <>
+                <Link
+                  to={`/academy/${getAcademyDetails(user!)?.academy_slug}`}
+                  target="_blank"
+                >
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="relative"
+                    title="معاينة الأكاديمية"
+                  >
+                    <Eye className="w-5 h-5" />
+                  </Button>
+                </Link>
+                <AddItemModal />
+              </>
+            )}
+
+            <NotificationsDropdown />
+          </div>
           <Searchbar />
           <Button
             variant="ghost"
